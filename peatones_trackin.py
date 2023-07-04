2d333f5b76cf46783d1670d1cb220fddb1319b3d
Carlos Hernandez <carloshinzunza2@gmail.com>
2023-07-03 17:49:32 -0700

    Detecta la trayectoria y guarda con un ID al objeto

--- peatones_trackin.py
+++ peatones_trackin.py
@@ -1,43 +1,37 @@
-import cv2
 import numpy as np
+import cv2
 from ultralytics import YOLO
-#from sort import Sort 
+from sort import Sort
 
-cap = cv2.VideoCapture('/Users/carloshernandez/Desktop/verano/pedestrian_dataset_1.mp4')
-model = YOLO('yolov8m.pt') #Calling the YOLO CNN
 
-#tracker = Sort() #inicializar objeto 
+cap = cv2.VideoCapture("/Users/carloshernandez/Desktop/verano/pedestrian_dataset_1.mp4") #Escogiendo el video
+model = YOLO("yolov8n.pt") #Llamando al modelo
 
-while True:
+tracker = Sort() #inicializando el objeto
+
+while cap.isOpened():
     ret, frame = cap.read()
+
     if not ret:
-        break
-    
-    results = model(frame)
-   
-    #results = model(frame,stream=True)
-    
+            break
 
-    '''for res in results:
-        boxes = res.boxes.xyxy.cpu().numpy().satyp(int) 
-        tracks = tracker.update(boxes)
-        tracks = tracks.astype(int) #Transformando a int
-        #print(tracks) #Se generan bboxes y ids
-        #print(res.boxes)
+    results = model(frame, stream=True)
 
-        for xmin,ymin,xmax,ymax, track_id in tracks:
-            cv2.putText(img=frame, text=f"Id: {track_id}", org=(xmin, ymin-10), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(0,255,0), thickness=2)
-            cv2.rectangle(img=frame, pt1=(xmin, ymin), pt2=(xmax, ymax), color=(0, 255, 0), thickness=2)
+    for res in results:
+            filtered_indices = np.where(res.boxes.conf.cpu().numpy() > 0.5)[0] #Que detecte cuando la confidencialidad es mayor a 0.5
+            boxes = res.boxes.xyxy.cpu().numpy()[filtered_indices].astype(int) #Muestras de tipo int 
+            tracks = tracker.update(boxes)
+            tracks = tracks.astype(int)
+            
+            for xmin, ymin, xmax, ymax, track_id in tracks: #Dibujando el rectangulo
+                cv2.putText(img=frame, text=f"Id: {track_id}", org=(xmin, ymin-10), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=2, color=(0,255,0), thickness=2)
+                cv2.rectangle(img=frame, pt1=(xmin, ymin), pt2=(xmax, ymax), color=(0, 255, 0), thickness=2)
 
-        break
-    '''
-    #Aqui habia un break que rompia el codigo de abajo
+        # frame = results[0].plot()
 
-    frame = results[0].plot() #Aqui grafico las bboxes
+    cv2.imshow("frame", frame)
 
-    cv2.imshow('dataset',frame) #Mostrando el vidio
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
 cap.release()
-cv2.destroyAllWindows()
